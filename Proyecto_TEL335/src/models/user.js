9e5f17d5bf944ba063dae536bace58494e12e807
MATÍAS IGNACIO AYALA <matias.ayala@sansano.usm.cl>
2023-06-24 01:04:46 -0400

    Se agrega login en backend, con verificacion en la bd

--- Proyecto_TEL335/src/models/user.js
+++ Proyecto_TEL335/src/models/user.js
@@ -43,4 +43,25 @@ UserSchema.statics.signup = async function (name, username, email, password){
     return user
 }
 
+//Static login
+UserSchema.statics.login = async function(email, password){
+    if(!email || !password){
+        throw Error('All fields must be filled')
+    }
+
+    const user = await this.findOne({email})
+    if (!user){
+        throw Error("Incorrect email or password")
+    }
+
+    const match = await bcrypt.compare(password, user.password)
+
+    if (!match){
+        throw Error("Incorrect email or password")
+    }
+
+    return user
+
+}
+
 module.exports = mongoose.model('User', UserSchema);
--- Proyecto_TEL335/src/routes/user.routes.js
+++ Proyecto_TEL335/src/routes/user.routes.js
@@ -11,7 +11,22 @@ const createToken = (_id) =>{
 
 //login
 router.post('/login', async (req, res) =>{
-    res.json({status: 'logged'});
+
+    const {email, password} = req.body;
+
+    try{
+        const user = await User.login(email, password)
+
+        //Create a token
+        const token = createToken(user._id)
+
+        res.status(200).json({email, token})
+
+    }catch(error){
+        
+        res.status(400).json({error: error.message})
+    }
+    
 });
 
 //signup
